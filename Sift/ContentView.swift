2d68b790e18045e93a0f2b610452f53d08e61051
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-05-23 00:51:19 +0100

    Initial Commit

---
+++ Sift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sift
+//
+//  Created by Ayodeji Osasona on 23/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sift/SiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiftApp.swift
+//  Sift
+//
+//  Created by Ayodeji Osasona on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
